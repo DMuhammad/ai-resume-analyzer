b107387a4ac74c39a7af0f5d116c3313cee9e80b
DzikriMuhammad <notfnd123@gmail.com>
2025-08-28 05:19:01 +0700

    add resume card to homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,13 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import type {
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+} from "react";
+import { resumes } from "constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,10 +21,17 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume: Resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
